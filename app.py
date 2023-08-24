10b86808688413ce9b2760519814f111d64cfedd
unknown <wilalexis76@gmail.com>
2023-08-24 09:05:18 -0600

    Se sube todo el proyecto

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/contact')  # Ruta para la página de contacto
+def contact():
+    return render_template('contact.html')
+
+@app.route('/login')  # Ruta para la página de login
+def login():
+    return render_template('login.html')
+
+@app.route('/register')  # Ruta para la página de registro
+def register():
+    return render_template('register.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
