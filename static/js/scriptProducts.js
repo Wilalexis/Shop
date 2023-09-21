205bb687ce097c4cffde3ee3d5d53c5d097b81e1
Jarod <jmejiac18@miumg.edu.gt>
2023-09-20 20:29:46 -0600

    Corrección de errores

---
+++ static/js/scriptProducts.js
@@ -0,0 +1,7 @@
+// Obtener el elemento del mensaje de éxito
+var mensajeExito = document.getElementById('mensaje-exito');
+
+// Ocultar el mensaje después de 3000 milisegundos (3 segundos)
+setTimeout(function() {
+    mensajeExito.style.display = 'none';
+}, 1000);
